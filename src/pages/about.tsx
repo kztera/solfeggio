f46696332a0475a5e1bbc9fadba64177c7a534ee
kztera <116504460+kztera@users.noreply.github.com>
2025-08-31 23:57:17 +0700

    feat: integrate generouted for routing and update dependencies

---
+++ src/pages/about.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1>Đây là trang About</h1>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import generouted from "@generouted/react-router/plugin";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), generouted(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
